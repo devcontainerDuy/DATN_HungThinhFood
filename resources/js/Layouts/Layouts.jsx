16318ada1c01b43fb735047949cccd0dbed066d3
tam986 <thanhtam1532@gmail.com>
2025-03-14 16:03:27 +0700

    template-update

--- resources/js/Layouts/Layouts.jsx
+++ resources/js/Layouts/Layouts.jsx
@@ -1,11 +1,96 @@
 import React, { useEffect } from "react";
 
-function Layouts() {
+function Layouts({ children }) {
     return (
         <>
-            <Header></Header>
+            <div className="container-fluid p-0">
+                <div className="row ">
+                    <div className="col-md-2 p-0">
+                        <nav
+                            class="sidebar d-flex flex-column p-3 text-white bg-dark"
+                            style={{ height: "100%" }}
+                        >
+                            <a
+                                href="#"
+                                class="text-white text-decoration-none d-flex align-items-center mb-3"
+                            >
+                                <img src="" alt="Logo" class="logo me-2" />
+                                <span class="fs-5 fw-bold">Admin</span>
+                            </a>
 
-            <Footer></Footer>
+                            <ul class="nav nav-pills flex-column">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">
+                                        <i class="bi bi-speedometer2 fs-3"></i>{" "}
+                                        <span> Dashboard</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">
+                                        <i class="bi bi-bar-chart fs-3"></i>{" "}
+                                        <span> Thống kê</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="" class="nav-link text-white">
+                                        <i class="bi bi-box-seam fs-3"></i>{" "}
+                                        <span> Quản lý sản phẩm</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">
+                                        <i class="bi bi-people fs-3"></i>{" "}
+                                        <span> Quản lý người dùng</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">
+                                        <i class="bi bi-box-seam fs-3"></i>{" "}
+                                        <span> Quản lý đơn hàng</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">
+                                        <i class="bi bi-people fs-3"></i>{" "}
+                                        <span> Quản lý nhà cung cấp</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">
+                                        <i class="bi bi-people fs-3"></i>{" "}
+                                        <span> Quản lý danh mục</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">
+                                        <i class="bi bi-people fs-3"></i>{" "}
+                                        <span> Quản lý khối lượng</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">
+                                        <i class="bi bi-people fs-3"></i>{" "}
+                                        <span> Quản lý nhân bánh</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">
+                                        <i class="bi bi-people fs-3"></i>{" "}
+                                        <span> Quản lý khuyến mãi</span>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link text-white">
+                                        <i class="bi bi-people fs-3"></i>{" "}
+                                        <span>Quản lý người dùng</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-md-10 p-0">{children}</div>
+                </div>
+            </div>
         </>
     );
 }
--- resources/js/Pages/Dashboard/Index.jsx
+++ resources/js/Pages/Dashboard/Index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-// import Chart from "chart.js/auto";
+import Layouts from "../../Layouts/Layouts";
+
 function Index() {
-    // useEffect(() => {
     //     let canvas = document.getElementById("myChart");
     //     let ctx = canvas.getContext("2d");
 
@@ -9,13 +9,12 @@ function Index() {
     //     const yValues = [];
 
     //     if (yValues.length === 0) {
-    //         canvas.style.display = "none";
-    //         let noDataMessage = document.createElement("p");
+    //         canvas.style.{{"isplay = "none";
+    // "}}       let noDataMessage = document.createElement("p");
     //         noDataMessage.innerText = "Không có dữ liệu để hiển thị!";
-    //         noDataMessage.style.color = "white";
-    //         noDataMessage.style.textAlign = "center";
-    //         noDataMessage.style.fontSize = "18px";
-    //         canvas.parentNode.appendChild(noDataMessage);
+    //         noDataMessage.style.{{"olor = "white";
+    //  "}}      noDataMessage.style".{{extAlign = "center";}}//         noDataMessage.style.{{"ontSize = "18px";
+    //"}}        canvas.parentNode.appendChild(noDataMessage);
     //         return;
     //     }
 
@@ -64,132 +63,173 @@ function Index() {
 
     return (
         <>
-            {" "}
-            <div
-                className="container-fluid p-4"
-                style={{ backgroundColor: "#161824" }}
-            >
-                <div className="container-fluid mt-4">
-                    <div className="container-fluid mb-4 p-0">
-                        <div className="container-fluid d-flex justify-content-end align-items-center flex-column p-0">
-                            <h1 className="text-white">Xin chào, Tâm</h1>
-                            <p className="text-white">
-                                Chúc bạn một ngày tốt lành
-                            </p>
-                        </div>
-                    </div>
-                    <h2 className="text-white mb-4">Trang Dashboard</h2>
+            <Layouts
+                children={
+                    <div
+                        className="container-fluid p-4"
+                        style={{ backgroundColor: "#161824" }}
+                    >
+                        <div className="container-fluid mt-4">
+                            <div className="container-fluid mb-4 p-0">
+                                <div className="container-fluid d-flex justify-content-end align-items-center flex-column p-0">
+                                    <h1 className="text-white">
+                                        Xin chào, Tâm
+                                    </h1>
+                                    <p className="text-white">
+                                        Chúc bạn một ngày tốt lành
+                                    </p>
+                                </div>
+                            </div>
+                            <h2 className="text-white mb-4">Trang Dashboard</h2>
 
-                    {/* Thống kê */}
-                    <div className="row text-white mb-4">
-                        {[
-                            {
-                                icon: "bi-pie-chart-fill",
-                                value: "10,000",
-                                label: "Lượt truy cập",
-                            },
-                            {
-                                icon: "bi-book",
-                                value: "14",
-                                label: "Tổng bài viết",
-                            },
-                            {
-                                icon: "bi-cake2-fill",
-                                value: "6",
-                                label: "Sản phẩm mới",
-                            },
-                            {
-                                icon: "bi-bag-check",
-                                value: "6",
-                                label: "Đơn hàng mới",
-                            },
-                        ].map((item, index) => (
-                            <div className="col-md-3 mb-3" key={index}>
-                                <div className="card bg-dark p-3">
-                                    <div className="d-flex justify-content-between align-items-center">
-                                        <div className="container-fluid">
-                                            <i
-                                                className={`bi ${item.icon} text-success fs-1`}
-                                            ></i>
+                            {/* <!-- Thống kê --> */}
+                            <div className="row text-white mb-4">
+                                <div className="col-md-3 mb-3">
+                                    <div className="card bg-dark p-3 ">
+                                        <div className="d-flex justify-content-between align-items-center ">
+                                            <div className="container-fluid">
+                                                <i className="bi bi-pie-chart-fill text-success fs-1"></i>
+                                            </div>
+                                            <div className="container-fluid  d-flex  align-items-start flex-column gap-1">
+                                                <span className="text-white fs-1">
+                                                    10,000
+                                                </span>
+                                                <span className="text-white ">
+                                                    Lượt truy cập
+                                                </span>
+                                                <span className="text-white ">
+                                                    <i className="bi bi-graph-up-arrow text-success"></i>
+                                                    Tăng
+                                                </span>
+                                            </div>
                                         </div>
-                                        <div className="container-fluid d-flex align-items-start flex-column gap-1">
-                                            <span className="text-white fs-1">
-                                                {item.value}
-                                            </span>
-                                            <span className="text-white">
-                                                {item.label}
-                                            </span>
-                                            <span className="text-white">
-                                                <i className="bi bi-graph-up-arrow text-success"></i>{" "}
-                                                Tăng
-                                            </span>
+                                    </div>
+                                </div>
+                                <div className="col-md-3 mb-3">
+                                    <div className="card bg-dark p-3">
+                                        <div className="d-flex justify-content-between align-items-center ">
+                                            <div className="container-fluid">
+                                                <i className="bi bi-book text-success fs-1"></i>
+                                            </div>
+                                            <div className="container-fluid  d-flex  align-items-start flex-column gap-1">
+                                                <span className="text-white fs-1">
+                                                    14
+                                                </span>
+                                                <span className="text-white ">
+                                                    Tổng bài viết
+                                                </span>
+                                                <span className="text-white ">
+                                                    <i className="bi bi-graph-up-arrow text-success"></i>{" "}
+                                                    Tăng
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3 mb-3">
+                                    <div className="card bg-dark p-3">
+                                        <div className="d-flex justify-content-between align-items-center ">
+                                            <div className="container-fluid">
+                                                <i className="bi bi-cake2-fill text-success fs-1"></i>
+                                            </div>
+                                            <div className="container-fluid  d-flex  align-items-start flex-column gap-1">
+                                                <span className="text-white fs-1">
+                                                    6
+                                                </span>
+                                                <span className="text-white ">
+                                                    Sản phẩm mới
+                                                </span>
+                                                <span className="text-white ">
+                                                    <i className="bi bi-graph-up-arrow text-success"></i>{" "}
+                                                    Tăng thêm
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-3 mb-3">
+                                    <div className="card bg-dark p-3">
+                                        <div className="d-flex justify-content-between align-items-center ">
+                                            <div className="container-fluid">
+                                                <i className="bi bi-bag-check text-success fs-1"></i>
+                                            </div>
+                                            <div className="container-fluid  d-flex  align-items-start flex-column gap-1">
+                                                <span className="text-white fs-1">
+                                                    6
+                                                </span>
+                                                <span className="text-white ">
+                                                    Đơn hàng mới
+                                                </span>
+                                                <span className="text-white ">
+                                                    <i className="bi bi-graph-up-arrow text-success"></i>{" "}
+                                                    Tăng thêm
+                                                </span>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        ))}
-                    </div>
+                            {/* 
+        <!-- Biểu đồ --> */}
+                            <div className="row mt-3 mb-3 gap-4 ">
+                                <div
+                                    className="col-md-9 bg-dark rounded p-3 ml-3"
+                                    style={{ width: "74.2%" }}
+                                >
+                                    <h5 className="text-white">
+                                        Thống kê số lượng đơn hàng
+                                    </h5>
+                                    <div className="container-fluid">
+                                        {/* <canvas
+                                            id="myChart"
+                                            style={{"width:100%;max-width:100"}}"
+                                        ></canvas> */}
+                                    </div>
+                                </div>
 
-                    {/* Biểu đồ */}
-                    <div className="row mt-3 mb-3 gap-4">
-                        <div
-                            className="col-md-9 bg-dark rounded p-3 ml-3"
-                            style={{ width: "74.2%" }}
-                        >
-                            <h5 className="text-white">
-                                Thống kê số lượng đơn hàng
-                            </h5>
-                            <div className="container-fluid">
-                                <canvas
-                                    id="myChart"
-                                    style={{ width: "100%", maxWidth: "100%" }}
-                                ></canvas>
+                                <div
+                                    className="col-md-3 card bg-dark rounded p-3"
+                                    style={{ width: "23%" }}
+                                >
+                                    <h4 className="text-white">
+                                        Danh sách khách hàng mới
+                                    </h4>
+                                    <p className="text-white">
+                                        Hiển thị các khách hàng mới đăng ký
+                                    </p>
+                                </div>
+                            </div>
+                            {/* <!-- bài viết và danh mục --> */}
+                            <div className="row mt-3 mb-3 gap-4 ">
+                                <div
+                                    className="col-md-9 bg-dark rounded p-3 ml-3"
+                                    style={{ width: "74.2%" }}
+                                >
+                                    <h5 className="text-white">
+                                        📊Thống kê số bài viết
+                                    </h5>
+                                    <div className="container-fluid">
+                                        @include('page.Baiviet')
+                                    </div>
+                                </div>
+
+                                <div
+                                    className="col-md-3 card bg-dark rounded p-3"
+                                    style={{ width: "23%" }}
+                                >
+                                    <h4 className="text-white">
+                                        Danh sách danh mục
+                                    </h4>
+                                    <p className="text-white">
+                                        Hiển thị số lượng sản phẩm đang có trong
+                                        danh mục
+                                    </p>
+                                </div>
                             </div>
                         </div>
-
-                        <div
-                            className="col-md-3 card bg-dark rounded p-3"
-                            style={{ width: "23%" }}
-                        >
-                            <h4 className="text-white">
-                                Danh sách khách hàng mới
-                            </h4>
-                            <p className="text-white">
-                                Hiển thị các khách hàng mới đăng ký
-                            </p>
-                        </div>
                     </div>
-
-                    {/* Bài viết và danh mục */}
-                    <div className="row mt-3 mb-3 gap-4">
-                        <div
-                            className="col-md-9 bg-dark rounded p-3 ml-3"
-                            style={{ width: "74.2%" }}
-                        >
-                            <h5 className="text-white">
-                                📊Thống kê số bài viết
-                            </h5>
-                            <div className="container-fluid">
-                                {/* Thay thế @include('page.Baiviet') bằng component React */}
-                                <p className="text-white">
-                                    Danh sách bài viết sẽ hiển thị ở đây
-                                </p>
-                            </div>
-                        </div>
-
-                        <div
-                            className="col-md-3 card bg-dark rounded p-3"
-                            style={{ width: "23%" }}
-                        >
-                            <h4 className="text-white">Danh sách danh mục</h4>
-                            <p className="text-white">
-                                Hiển thị số lượng sản phẩm đang có trong danh
-                                mục
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                }
+            />
         </>
     );
 }
